6c2b63bf5dd90e30938946a5944baff6b7fe7aee
Evimar Principal <evimar.principal@gmail.com>
2021-08-01 14:52:01 -0400

    add a docstring and add function accepts iterable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from bisect import bisect
-from typing import List, Dict
+from typing import List, Dict, Iterable
 from functools import wraps
 
 
@@ -55,18 +55,43 @@ class Stats:
 
 
 class DataCapture(object):
-    """ Captures the input and generate stats of small integers."""
+    """ Captures the input and generate stats of small integers.
+
+    >>> datacapture = DataCapture()
+    >>> datacapture.add(10)
+    >>> datacapture.add(1)
+    >>> datacapture.add(5)
+    >>> datacapture._sorted_keys
+    [1, 5, 10]
+    >>> datacapture.add(1)
+    >>> datacapture._sorted_keys
+    [1, 5, 10]
+    >>> datacapture._data[1]
+    2
+    >>> datacapture._data[5]
+    1
+    >>> datacapture._data[8]
+    0
+    >>> stats = datacapture.build_stats()
+    >>> stats.less(1)
+    0
+    """
 
     def __init__(self) -> None:
         self._data = Counter()
         self._sorted_keys = list()
 
-    def add(self, number: int):
-        self._data[number] += 1
-        if number not in self._sorted_keys:
-            index = bisect(self._sorted_keys, number)
-            self._sorted_keys.insert(index, number)
+    def add(self, *numbers: Iterable):
+        self._data += Counter(numbers)
+        for number in numbers:
+            if number not in self._sorted_keys:
+                index = bisect(self._sorted_keys, number)
+                self._sorted_keys.insert(index, number)
 
     def build_stats(self) -> Stats:
         return Stats(self._data, self._sorted_keys)
 
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
